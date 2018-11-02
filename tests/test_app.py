eb91d19717a844793acaae4f4c04e58d7cacc3e4
LucasPenido <lpenidoa@me.com>
2018-11-02 17:35:44 -0300

    Adiciona teste de importação de arquivo pelo filtro específico de data.

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,9 +3,7 @@ import json
 from .schemas import assert_valid_schema, get_pingout_schema, get_wrong_pingout_schema, \
                      create_file_ping, failure_create_file_ping
 from datetime import datetime, timedelta
-import sys                     
-                     
-
+import sys
 
 def test_return_200_on_root(client):
     """ Return status code 200 on root url"""
@@ -16,7 +14,7 @@ def test_return_200_on_root(client):
 def test_create_pingout(client):
     """ Return status code 200 on root url"""
     response = client.post('/create-pingout')
-    assert response.status_code == 201    
+    assert response.status_code == 201
 
 
 def test_ping(client):
@@ -28,7 +26,7 @@ def test_ping(client):
 
 def test_failure_ping(client):
     response = client.post('/' + "1234" + "/ping" )
-    get_data = json.loads(response.data)    
+    get_data = json.loads(response.data)
 
     assert response.status_code == 400
     assert assert_valid_schema(get_data, get_wrong_pingout_schema) == None
@@ -38,7 +36,7 @@ def test_get_pings(client):
     res = client.post('/create-pingout')
     post_data = json.loads(res.data)
     response = client.get('/' + post_data['uuid'] )
-    get_data = json.loads(response.data)    
+    get_data = json.loads(response.data)
 
     assert response.status_code == 200
     assert assert_valid_schema(get_data, get_pingout_schema) == None
@@ -46,7 +44,7 @@ def test_get_pings(client):
 
 def test_failure_get_pings(client):
     response = client.get('/' + '1234' )
-    get_data = json.loads(response.data)    
+    get_data = json.loads(response.data)
 
     assert response.status_code == 400
     assert assert_valid_schema(get_data, get_wrong_pingout_schema) == None
@@ -79,7 +77,7 @@ def test_failure_export_range_csv(client):
 def test_export_specific_csv(client):
     res = client.post('/create-pingout')
     post_data = json.loads(res.data)
-    date = datetime.today().strftime('%Y-%m-%d')    
+    date = datetime.today().strftime('%Y-%m-%d')
 
     ping = client.post('/' + post_data['uuid'] + '/ping')
     response = client.get('/' + post_data['uuid'] + '/filter_specific?date=' + date)
@@ -97,3 +95,17 @@ def test_failure_export_specific_csv(client):
 
     assert response.status_code == 400
     assert assert_valid_schema(get_data, failure_create_file_ping) == None
+
+def test_create_file_by_export_specific_csv(client):
+    res = client.post('/create-pingout')
+    post_data = json.loads(res.data)
+
+    date = datetime.today().strftime('%Y-%m-%d')
+    client.post('/' + post_data['uuid'] + '/ping')
+    client.get('/' + post_data['uuid'] + '/filter_specific?date=' + date)
+
+    file = open('../app/files/' + post_data['uuid'] + '.csv' , 'r')
+    expected = ",0\n" + date + ",1\n"
+
+    assert expected == file.read()
+    
